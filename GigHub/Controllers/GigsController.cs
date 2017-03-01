5b6b9457d9f04d0952b342e214db60b6c4f21fa1
David Barth <davidbarth80@gmail.com>
2017-03-01 03:18:26 +0000

    gigs controller, bootstrap view

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+        
+        public GigsController()
+        {
+            this._context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                //get list from context
+                Genres = _context.Genres.ToList() 
+            };
+
+            return View(viewModel);
+        }
+    }
+}
