d3f678903353a82957c0da2a7ba9622a6d5a35e6
David Barth <davidbarth80@gmail.com>
2017-03-01 00:34:21 +0000

    create gig and genre model

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //EF will know about Gig class - domain model
+        //EF will know about genres through gig as gig has a reference to it 
+        public DbSet<Gig> Gigs { get; set; }
+
+        //to query genres 
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+
+        public int Id { get; set; }
+
+
+        //because it's nullable by convention 
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)] //limit length instead of MAX by convention
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+
+    
+}
