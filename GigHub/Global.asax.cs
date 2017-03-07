7d573a0203fbc670d3802e27255c66451a4d15d0
David Barth <davidbarth80@gmail.com>
2017-03-07 15:21:09 +0000

    implementing RESTful Web API for resources /api/attendances

--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +13,10 @@ namespace GigHub
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            //when creating a WEB API controller this line should be added
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
